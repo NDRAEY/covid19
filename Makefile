84b65eae26dcd53ef4a9731fda296ddf0666d7bf
AndreyTheHacker <andrejpavlenko666@gmail.com>
2020-04-25 07:27:47 +0000

    added --help flag

--- Makefile
+++ Makefile
@@ -1,5 +1,24 @@
+VER 	= 	1.1
+REL 	= 	1
+file 	=	main.py
+
 all:
+	echo "Making main.py executable"
 	chmod +x main.py
+	
+build-debian:
+	echo "Copying files..."
+	cp $(file) debian/build/covid19_$(VER)-$(REL)/usr/local/bin/covid19
+	chmod +x debian/build/covid19_$(VER)-$(REL)/usr/local/bin/covid19
+	echo "\e[4mBuilding\e[0m debian package"
+	dpkg-deb --build debian/build/covid19_$(VER)-$(REL)
+	mv debian/build/covid19_$(VER)-$(REL).deb debian/
+	echo "\e[32m\e[1mcovid19_$(VER)-$(REL).deb in debian/ folder\e[0m" 
 
 install:
+	echo "Installing main.py into /usr/local/lib/covid19"
 	cp main.py /usr/local/bin/covid19
+	chmod +x /usr/local/bin/covid19
+	
+uninstall:
+	rm /usr/local/bin/covid19
--- main.py
+++ main.py
@@ -1,10 +1,40 @@
+#!/usr/bin/python
+
 import requests
-from bs4 import * 
+from bs4 import *
+import sys
+import os
 
 raw_data = requests.get("http://www.worldometers.info/coronavirus").content
 # getting raw data from Worldometers.info
 data = BeautifulSoup(raw_data, features='lxml')
 # Initialising BeatifulSoup
-parsed_data = str(str(data.find('title','')).replace("<title>Coronavirus Update (Live)","")).replace(': ','').replace('Cases and ','\n').replace('Deaths from COVID-19 Virus Pandemic - Worldometer</title>','')
-print(parsed_data)
+parsed_data = str(str(data.find('title','')).replace("<title>Coronavirus Update (Live)","")).replace(': ','').replace('Cases and ','cases\n').replace('Deaths from COVID-19 Virus Pandemic - Worldometer</title>','deaths ');
+
+def write_temps():
+	_file = open(".tmp","w+");
+	_file.write(parsed_data);
+	_file.close();
+
+def get_data():
+	print(parsed_data + '\r');
+#############################################
+try:
+	if(sys.argv[1] == '--create-temps') : 
+		write_temps();
+		get_data();
+		
+	elif(sys.argv[1] == '--help') : 
+		print('''Usage: covid19 [optional args]
+
+--create-temps - creates the .tmp file with data
+		''');
+	elif(sys.argv[1] == '--numbers-only') : 
+		print(parsed_data.replace(" cases","").replace(" deaths",""));
+	
+except IndexError:
+	os.system("true");
+	get_data();
+##############################################
+
 
